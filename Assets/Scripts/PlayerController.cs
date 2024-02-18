2159c6e45bad6d4b56f6e1fecaef4b5ef62a7d65
Zihan Yang <ws704603301@gmail.com>
2024-02-18 15:08:26 -0500

    Tried two more diffent way of access the surface effector 2d in the level sprite shape!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,22 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float torqueAmount = 50.0f;
     [SerializeField] float moveSpeed = 20.0f;
     [SerializeField] float boostSpeed = 40.0f;
+    // [SerializeField] SurfaceEffector2D surfaceEffector2D;  // Approach 1: Assign the SurfaceEffector2D in the Inspector
+    [SerializeField] GameObject levelSprite;  // Approach 2: Assign the gameobject with SurfaceEffector2D in the Inspector
+
+
     Rigidbody2D rb2d;
-    SurfaceEffector2D surfaceEffector2D;
+    SurfaceEffector2D surfaceEffector2D;  // Approach 2
+    // SurfaceEffector2D surfaceEffector2D; // Approach 3: Use FindObjectOfType to get the SurfaceEffector2D
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+        // surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();  // Approach 3
+        surfaceEffector2D = levelSprite.GetComponent<SurfaceEffector2D>(); // Approach 2
+
     }
 
     // Update is called once per frame
